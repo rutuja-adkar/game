28ba98c1a7d9ed33a114fcf16de7b0bfc6cf3588
rutuja adkar <rutujaadkargmail.com>
2025-01-16 11:48:00 +0530

    added code for clock tictatoe and connectgame

---
+++ clock.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+root=Tk()
+root.title("clock")
+def time():
+    string=strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+label=Label(root, font=("ds-digital bold italic",80),background="yellow",foreground="black")
+label.pack(anchor='center')
+time()
+mainloop()
